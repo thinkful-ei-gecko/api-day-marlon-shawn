273ac84466a2516c9e1be5ec149eda069c09bf64
Shawn Collette <shawn.collette@gmail.com>
2019-08-23 12:11:34 -0600

    driver switch

--- scripts/api.js
+++ scripts/api.js
@@ -3,10 +3,20 @@
 
 const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/shawn';
-  let getItems = function() {
+  let getItem = function() {
     return fetch(`${BASE_URL}/items`);
   };
+  let createItem = function(name) {
+    let newItem =JSON.stringify({name: name});
+    let fetchObj = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: newItem
+    };
+    return fetch(`${BASE_URL}/items`, fetchObj);
+  };
   return {
-    getItems,
+    getItem,
+    createItem,
   };
 })();
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
+/* eslint-disable no-console */
 /* eslint-disable no-undef */
 'use strict';
-/* global shoppingList, store, Item */
-// eslint-disable-next-line no-unused-vars
-
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
@@ -10,17 +8,3 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
-
-api.getItems()
-  .then(res => res.json())
-  .then(items => console.log(items));
-
-// console.log(api.BASE_URL);
-// console.log(api.getItems());
-
-// function callByBreed(breed) {
-//   fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-//     .then(response => response.json())
-//     .then(responseJson => displayBreedResult(responseJson));
-
-// You'll need to update your index.js call to api.getItems() to first resolve the response datastream with .json() and then log out the data from the second resolved promise
